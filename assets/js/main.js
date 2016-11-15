57433c31dbf6bb26fd848b91e439381a4f6a5024
technovisioncs <technovisioncs@gmail.com>
2016-11-15 19:58:44 +0530

    mobile responsiveness navbar added

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,16 @@
 				$('body').toggleClass('body-push-toleft');
 				$('#theMenu').toggleClass('menu-open');
 			});
+			$('a[href^="#"]').on('click',function (e) {
+			    e.preventDefault();
 
+			    var target = this.hash,
+			    $target = $(target);
+
+			    $('html, body').stop().animate({
+			        'scrollTop': $target.offset().top
+			    }, 900, 'swing', function () {
+			        window.location.hash = target;});
+			});
 
 })(jQuery)
